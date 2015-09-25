fcf466baf3e49673c9d2b85f6239f52bb2191954
Bob Hiltner <bobhilt@gmail.com>
2015-09-25 00:37:16 -0700

    Add image uploader and show recipe images.

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -4,4 +4,13 @@ class Recipe < ActiveRecord::Base
   validates :name, presence: true, length: {minimum: 5, maximum: 100}
   validates :summary, presence: true, length: {minimum: 5, maximum: 100}
   validates :description, presence: true, length: { maximum: 3000 }
+  mount_uploader :picture, PictureUploader
+  validate :picture_size
+  
+  private
+    def picture_size
+      if picture.size > 5.megabytes
+        errors.add(:picture, "maximum size 5mb")
+      end
+    end
 end
